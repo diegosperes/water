b95831680c9252cb2184e27ddb98d5ac9f9a851b
Diego Peres <speres.diego@gmail.com>
2018-11-14 18:57:35 -0200

    Implementing get method in api handler

--- tests/test_api_handler.py
+++ tests/test_api_handler.py
@@ -1,37 +1,75 @@
 import json
 from tornado.testing import AsyncHTTPTestCase, gen_test
+from tornado.httpclient import HTTPClientError
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 from wormhole.model import Model
 from wormhole.server import make_app
+from tests import DatabaseTestCase
 
 
-database = 'database'
-collection = 'collection'
-
-
-class ModelTestCase(AsyncHTTPTestCase):
-    
-    client = MongoClient()[database][collection]
+class ModelTestCase(DatabaseTestCase, AsyncHTTPTestCase):
 
     def get_app(self):
         return make_app()
-    
-    def setUp(self):
-        super().setUp()
-        self.model = Model(database, collection)
-        _id = self.client.insert_one(self.model.data).inserted_id
-        data = self.client.find_one({'_id': _id})
-        self.model = Model(database, collection, data=data)
-    
+
     @gen_test
     async def test_get_data(self):
-        request = await self.request(self.model.data['_id'])
+        model = self.model_factory()
+        request = await self.request(model.data['_id'])
         result = json.loads(request.body)
 
         self.assertEqual(200, request.code)
-        self.assertEqual(3, len(result))
+        self.assertEqual(4, len(result))
 
-    async def request(self, suffix):
-        url = self.get_url('/{0}/{1}/{2}'.format(database, collection, suffix))
-        return await self.http_client.fetch(url)
+    @gen_test
+    async def test_get_data_when_does_not_exist(self):
+        request = await self.request(ObjectId())
+        result = json.loads(request.body)
+
+        self.assertEqual(404, request.code)
+        self.assertEqual(0, len(result))
+
+    @gen_test
+    async def test_get_data_list_when_does_not_exist(self):
+        request = await self.request()
+        result = json.loads(request.body)
+
+        self.assertEqual(404, request.code)
+        self.assertEqual(3, len(result))
+        self.assertEqual(0, len(result['result']))
+
+    @gen_test
+    async def test_paginated_data_without_page_value(self):
+        [self.model_factory() for i in range(15)]
+        request = await self.request()
+        result = json.loads(request.body)
+
+        self.assertEqual(200, request.code)
+        self.assertEqual(10, len(result['result']))
+
+    @gen_test
+    async def test_paginated_data_with_page_value(self):
+        [self.model_factory() for i in range(15)]
+        request = await self.request('?page=2')
+        result = json.loads(request.body)
+
+        self.assertEqual(200, request.code)
+        self.assertEqual(5, len(result['result']))
+
+    @gen_test
+    async def test_paginated_data_with_wrong_page_value(self):
+        [self.model_factory() for i in range(15)]
+        request = await self.request('?page=a')
+        result = json.loads(request.body)
+
+        self.assertEqual(400, request.code)
+        self.assertEqual(0, len(result['result']))
+
+    async def request(self, suffix=''):
+        try:
+            url = self.get_url('/{0}/{1}/{2}'.format(self.database, self.collection, suffix))
+            return await self.http_client.fetch(url)
+        except HTTPClientError as error:
+            return error.response
--- wormhole/api_handler.py
+++ wormhole/api_handler.py
@@ -1,10 +1,35 @@
 from tornado.web import RequestHandler
 
 from wormhole.model import Model
+from wormhole.model_list import ModelList
 from wormhole import json
 
 
 class ApiHandler(RequestHandler):
     async def get(self, database, collection, _id):
+        if _id:
+            view, status_code = await self._get_data(database, collection, _id)
+        else:
+            view, status_code = await self._get_list(database, collection)
+
+        self.set_status(status_code)
+        data = json.dumps(view)
+        self.finish(data)
+
+    async def _get_list(self, database, collection):
+        model = ModelList(database, collection)
+        try:
+            page = self.get_argument('page', 1)
+            page = int(page)
+        except ValueError:
+            return model.view, 400
+
+        await model.find(page)
+        status_code = 200 if model.has_result() else 404
+        return model.view, status_code
+
+    async def _get_data(self, database, collection, _id):
         model = await Model.find(database, collection, _id)
-        self.finish(json.dumps(model.view))
+        if model:
+            return model.view, 200
+        return {}, 404
